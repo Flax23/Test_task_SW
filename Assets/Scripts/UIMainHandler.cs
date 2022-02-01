c6b47bebf35776d80785bcfc0131a2de40061600
ilia_sokolov <ilia_sokolov90@mail.ru>
2022-02-02 00:11:07 +0300

    6

--- Assets/Scripts/UIMainHandler.cs
+++ Assets/Scripts/UIMainHandler.cs
@@ -8,40 +8,72 @@ public class UIMainHandler : MonoBehaviour
 {
     [SerializeField] private RectTransform formsPanel;
     [SerializeField] private RectTransform mainColor;
+    [SerializeField] private RectTransform colorPanel;
     [SerializeField] private Sprite color;
     [SerializeField] private Sprite selectedColor;
-    [SerializeField] private Image targetButtonImage;
     [SerializeField] private GameObject targetButton;
-    [SerializeField] private Button selectColor;
+    [SerializeField] private Button selectColorButton;
     [SerializeField] private GameObject activForm;
+    [SerializeField] private Color[] curentColors = new Color[3];
+    [SerializeField] private List<Color> ColorPalette = new List<Color>();
+     
 
-    public void SelectColor()
+    private void Start()
     {
-        selectColor = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
-        targetButton.transform.GetChild(0).GetComponent<Image>().color = selectColor.colors.normalColor;
-        targetButton.transform.GetChild(0).GetComponent<Image>().gameObject.SetActive(true);
+        activForm = formsPanel.GetChild(0).gameObject;
+        InitColors();
+        InitColorsPalette();
+        targetButton = mainColor.GetChild(0).gameObject;
+        targetButton.GetComponent<Image>().sprite = selectedColor;
+    }
 
-        for (int i = 0; i < formsPanel.childCount; i++)
+    public void RandomColors()
+    {            
+        Color[] randomColor = new Color[3];
+        for (int i = 0; i < randomColor.Length; i++)
         {
-            if (formsPanel.GetChild(i).gameObject.activeInHierarchy)
+           randomColor[i] = ColorPalette[Random.Range(0, ColorPalette.Count)];
+
+            for (int j = 0; j < curentColors.Length; j++)
             {
-                activForm = formsPanel.GetChild(i).gameObject;
+                if (randomColor[i].Equals(curentColors[j])) i--;
             }
         }
+        
+        activForm.transform.Find("Layer_1").GetComponent<Image>().color = randomColor[0];
+        activForm.transform.Find("Layer_2").GetComponent<Image>().color = randomColor[1];
+        activForm.transform.Find("Layer_3").GetComponent<Image>().color = randomColor[2];
+        InitColors();
+    }
+
+    public void SelectColor()
+    { 
+        selectColorButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+
+        for (int i = 0; i < curentColors.Length; i++)
+        {
+            if (selectColorButton.colors.normalColor.Equals(curentColors[i])) return;
+        }
+                       
+        targetButton.transform.GetChild(0).GetComponent<Image>().color = selectColorButton.colors.normalColor;
 
         switch (targetButton.name)
         {
-            case "ButtonLeft":
-                activForm.transform.Find("Layer_1").GetComponent<Image>().color = selectColor.colors.normalColor;
-                break;
-            case "ButtonMidlle":
-                activForm.transform.Find("Layer_2").GetComponent<Image>().color = selectColor.colors.normalColor;
-                break;
-            case "ButtonRight":
-                activForm.transform.Find("Layer_3").GetComponent<Image>().color = selectColor.colors.normalColor;
-                break;
-        }
+             case "ButtonLeft":
+                 activForm.transform.Find("Layer_1").GetComponent<Image>().color = selectColorButton.colors.normalColor;
+                 curentColors[0] = selectColorButton.colors.normalColor;
+                 break;
+             case "ButtonMidlle":
+                 activForm.transform.Find("Layer_2").GetComponent<Image>().color = selectColorButton.colors.normalColor;
+                 curentColors[1] = selectColorButton.colors.normalColor;
+                 break;
+             case "ButtonRight":
+                 activForm.transform.Find("Layer_3").GetComponent<Image>().color = selectColorButton.colors.normalColor;
+                 curentColors[2] = selectColorButton.colors.normalColor;
+                 break;
+         }
             
+          
     }
 
     public void FormsColorPart()
@@ -68,6 +100,8 @@ public class UIMainHandler : MonoBehaviour
                 if (i >= formsPanel.childCount) i--;
 
                 formsPanel.GetChild(i).gameObject.SetActive(true);
+                activForm = formsPanel.GetChild(i).gameObject;
+                InitColors();
                 return;
             }
         }
@@ -85,8 +119,28 @@ public class UIMainHandler : MonoBehaviour
                 if (i < 0) i++;
 
                 formsPanel.GetChild(i).gameObject.SetActive(true);
+                activForm = formsPanel.GetChild(i).gameObject;
+                InitColors();
                 return;
             }
+        }
+    }
+
+    public void InitColors()
+    {
+        for (int i = 0; i < mainColor.childCount; i++)
+        {
+            targetButton = mainColor.GetChild(i).gameObject;
+            targetButton.transform.GetChild(0).GetComponent<Image>().color = activForm.transform.GetChild(i + 1).GetComponent<Image>().color;
+            curentColors[i] = targetButton.transform.GetChild(0).GetComponent<Image>().color;
+        }
+    }
+
+    public void InitColorsPalette()
+    {
+        for (int i = 0; i < colorPanel.childCount; i++)
+        {
+            ColorPalette.Add(colorPanel.GetChild(i).gameObject.GetComponent<Button>().colors.normalColor);
         }
     }
 }
